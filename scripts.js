d4b408637e0ed4d9da2464234ab32ba0ff44e6a9
Stephan Mühl <31169771+Blueforcer@users.noreply.github.com>
2023-06-27 21:17:00 +0200

    update

--- scripts.js
+++ scripts.js
@@ -1,13 +1,35 @@
 const recipes = {
-    mojito: 'Zutaten: Minze, Limette, Zucker, Eis, Weißer Rum, Sodawasser.<br>Zubereitung: Limetten und Minze muddeln, Zucker hinzufügen, mit Eis auffüllen, Rum hinzufügen und mit Sodawasser auffüllen.',
-    latte: 'Zutaten: Espresso, Milch.<br>Zubereitung: Einen Espresso zubereiten und in ein Glas geben. Milch aufschäumen und vorsichtig über den Espresso gießen.',
-    // Weitere Rezepte hier
+    gintonic: {
+        ingredients: '<ul><li>Bombay Sapphire Gin</li><li>Thomas Henry Tonic Water</li><li>Limette</li><li>Eiswürfel</li></ul>',
+        image: 'gintonic.png'
+    },
+    cubalibre: {
+        ingredients: '<ul><li>Havana</li><li>Cola</li><li>Limette</li><li>Eiswürfel</li></ul>',
+        image: 'cubalibre.png'
+    },
+    aperol1: {
+        ingredients: '<ul><li>Sekt</li><li>Aperol</li><li>Sodawasser</li><li>Orangenscheibe</li><li>Eiswürfel</li></ul>',
+        image: 'aperol1.png'
+    },
+    aperol2: {
+        ingredients: '<ul><li>Orange Spritz Sirup</li><li>Alkoholfreier Sekt</li><li>Sodawasser</li><li>Orangenscheibe</li><li>Eiswürfel</li></ul>',
+        image: 'aperol2.png'
+    },
+    hugo: {
+        ingredients: '<ul><li>Sekt</li><li>Sodawasser</li><li>Holunderblütensirup</li><li>Limette</li><li>Minzzweig</li><li>Eiswürfel</li></ul>',
+        image: 'hugo.png'
+    },
+    hugo2: {
+        ingredients: '<ul><li>Alkoholfreier Sekt</li><li>Holunderblütensirup</li><li>Zitronensaft</li><li>Limette</li><li>Minzzweig</li><li>Eiswürfel</li></ul>',
+        image: 'hugo2.png'
+    },
 };
 
 function showRecipe(drink) {
     const recipeElement = document.getElementById('recipe');
     const modal = document.getElementById('recipeModal');
-    recipeElement.innerHTML = recipes[drink];
+    const recipe = recipes[drink];
+    recipeElement.innerHTML = `<img src="${recipe.image}" alt="${drink}" class="w-64 mx-auto mb-4"><div>${recipe.ingredients}</div>`;
     modal.classList.remove('hidden');
 }
 
